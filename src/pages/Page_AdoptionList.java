1513375731e12183c6fde80de22d9fa7e868908b
Andrew Parra <andrew.parra@wellspring.com>
2019-01-03 12:25:35 -0600

    Updated some logic to View Puppy Details

--- src/pages/Page_AdoptionList.java
+++ src/pages/Page_AdoptionList.java
@@ -1,8 +1,18 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import data_objects.PuppyListItem;
 
 public class Page_AdoptionList extends Page_Base {
+    
+    private static final By PUPPY_LIST = By.className("puppy_list");
+    private static final By NEXT_PAGE = By.className("next_page");
 
     public Page_AdoptionList(WebDriver driver) {
         super(driver);
@@ -18,8 +28,28 @@ public class Page_AdoptionList extends Page_Base {
     }
 
     public Page_PuppyDetails viewPuppiesDetails(String puppiesName) {
-        // TODO Auto-generated method stub
+        PuppyListItem puppy;
+        do {
+            for(WebElement puppyElement: driver.findElements(PUPPY_LIST)) {
+                puppy = new PuppyListItem(puppyElement);
+                System.out.println(puppy.toString());
+                if (puppy.getName().equals(puppiesName)) {
+                    puppy.clickViewDetails();
+                }
+            }
+        } while(nextPage());
+        
         return pages.PuppyDetails();
     }
 
+    private Boolean nextPage() {
+        WebElement next = driver.findElement(NEXT_PAGE);
+        if (next.isEnabled()) {
+            next.click();
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
 }
