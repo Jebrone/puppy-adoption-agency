09265170acb6331aff0a3c35662deb88b87501df
Andrew Parra <andrew.parra@wellspring.com>
2019-01-03 12:25:07 -0600

    Added a puppy Object

---
+++ src/data_objects/PuppyListItem.java
@@ -0,0 +1,52 @@
+package data_objects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class PuppyListItem {
+    private WebElement puppyListElement;
+    
+    private static final By NAME = By.cssSelector("[class='name'] *");
+    private static final By BREED = By.cssSelector("[class='details'] *:nth-child(1)");
+    private static final By GENDER = By.cssSelector("[class='details'] *:nth-child(2)");
+    private static final By FORM = By.cssSelector("[class='view'] form");
+    private static final By VIEW_DETAILS_BUTTON = By.cssSelector("[class='view'] [value='View Details']");
+    
+    private String name, breed, gender, url;
+    
+    public PuppyListItem(WebElement puppyListElement) {
+        this.puppyListElement = puppyListElement;
+        name = puppyListElement.findElement(NAME).getText();
+        breed = puppyListElement.findElement(BREED).getText();
+        gender = puppyListElement.findElement(GENDER).getText();
+        url = puppyListElement.findElement(FORM).getAttribute("action");
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBreed() {
+        return this.breed;
+    }
+    
+    public String getGender() {
+        return this.gender;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+    
+    public int getId() {
+        return url.charAt(url.length() -1);
+    }
+    
+    public void clickViewDetails() {
+        puppyListElement.findElement(VIEW_DETAILS_BUTTON).click();
+    }
+    
+    public String toString() {
+        return "[Name: " + this.name + "][Breed: " + this.breed + "][Gender: " + this.gender + "][ID: " + getId() + "]";
+    }
+}
