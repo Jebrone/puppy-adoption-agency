d9fb1260ca04ac70a4f84256cc80c4fbc07f2edf
Jebrone <jebrone@hotmail.com>
2019-01-04 00:35:52 -0600

    Added Drivers to Project for windows and Mac

--- src/tests/TC_Base.java
+++ src/tests/TC_Base.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import pages.Page_Base;
 
@@ -15,9 +16,21 @@ public class TC_Base {
     protected Page_Base basePage;
 
     @BeforeTest
-    public void setup_InitializeDriver() {
+    @Parameters({"Operating System"})
+    public void setup_InitializeDriver(String os) {
+    	switch(os.toLowerCase()) {
+    		case "windows":
+    			System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+    			break;
+    		case "mac":
+    			System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+    			break;
+    	}
+    	
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        
         basePage = new Page_Base(driver);
     }
     
